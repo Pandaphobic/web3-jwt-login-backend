3de7f713b93f1fa988740de2b1d04b04a27f9f00
Chris Steffes <c.steffes@live.ca>
2021-06-26 21:04:44 -0500

    bcrypted password handling

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router()
 const User = require("../model/User")
 const { registerValidation, loginValidation } = require("../validation")
+const bcrypt = require("bcryptjs")
 
+// Registration
 router.post("/register", async (req, res) => {
   // Validation
   const { error } = registerValidation(req.body)
@@ -13,22 +15,39 @@ router.post("/register", async (req, res) => {
   })
   if (emailExists) return res.status(400).send("Email already exists!")
 
+  // Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashPassword = await bcrypt.hash(req.body.password, salt)
+
   // Create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashPassword
   })
   try {
     const savedUser = await user.save()
-    res.send(savedUser)
+    res.send({ user: user._id })
   } catch (err) {
     res.status(400).send(err)
   }
 })
 
-router.post("/login", (req, res) => {
-  res.send("Login")
+// Login
+router.post("/login", async (req, res) => {
+  // Validation
+  const { error } = loginValidation(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  // Check if user alerady exists
+  const user = await User.findOne({
+    email: req.body.email
+  })
+  if (!user) return res.status(400).send("Email incorrect.")
+  const validPass = await bcrypt.compare(req.body.password, user.password)
+  if (!validPass) return res.status(400).send("Password incorrect")
+
+  res.send("Logged in!")
 })
 
 module.exports = router
