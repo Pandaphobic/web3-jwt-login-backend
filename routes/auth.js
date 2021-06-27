362e38f0018c5704f8edf4655c2767313e5334d1
Chris Steffes <c.steffes@live.ca>
2021-06-26 20:52:16 -0500

    check if user exist @registration

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,17 @@ const User = require("../model/User")
 const { registerValidation, loginValidation } = require("../validation")
 
 router.post("/register", async (req, res) => {
+  // Validation
   const { error } = registerValidation(req.body)
   if (error) return res.status(400).send(error.details[0].message)
+
+  // Check if user alerady exists
+  const emailExists = await User.findOne({
+    email: req.body.email
+  })
+  if (emailExists) return res.status(400).send("Email already exists!")
+
+  // Create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
