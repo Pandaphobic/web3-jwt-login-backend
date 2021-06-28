f69b591764080e6778fca0d5810f0702f4941bbd
Chris Steffes <c.steffes@live.ca>
2021-06-28 08:15:13 -0500

    retrieve only owned profile

--- routes/profile.js
+++ routes/profile.js
@@ -9,10 +9,16 @@ TODO:
  - Only retrieve owned sensitive information
  - Error handling
 */
-module.exports = router.post("/:userID", verify, async (req, res) => {
+module.exports = router.get("/:userID", verify, async (req, res) => {
+  // Get userID of request
+  const reqUserId = req.user._id
+
   // Check if user alerady exists
-  const emailExists = await User.findOne({
+  const idExists = await User.findOne({
     _id: req.params.userID
   })
-  if (emailExists) return res.status(200).send(emailExists)
+  // If requesting user is the the same as the one we're getting
+  // return all the information. Else: Access Denied
+  if (idExists._id == reqUserId) return res.status(200).send(idExists)
+  else return res.status(401).send("Access Denied")
 })
