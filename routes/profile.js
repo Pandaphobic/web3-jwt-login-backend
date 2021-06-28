93c9b462d76c40b575451b655e13c704b6187fae
Chris Steffes <c.steffes@live.ca>
2021-06-28 08:29:19 -0500

    error handling

--- routes/profile.js
+++ routes/profile.js
@@ -2,23 +2,30 @@ const router = require("express").Router()
 const verify = require("./verifyToken")
 const User = require("../model/User")
 
-// If authenticated, return entire user information for ID specified
+// Used to return profile data for only the currently logged in user
 
 /*
 TODO:
- - Only retrieve owned sensitive information
- - Error handling
+ [x] Only retrieve owned sensitive information
+ [x] Error handling
 */
 module.exports = router.get("/:userID", verify, async (req, res) => {
   // Get userID of request
   const reqUserId = req.user._id
+  let idExists
 
   // Check if user alerady exists
-  const idExists = await User.findOne({
-    _id: req.params.userID
-  })
-  // If requesting user is the the same as the one we're getting
-  // return all the information. Else: Access Denied
-  if (idExists._id == reqUserId) return res.status(200).send(idExists)
-  else return res.status(401).send("Access Denied")
+  try {
+    idExists = await User.findOne({
+      _id: req.params.userID
+    })
+    // If requesting user is the the same as the one we're getting
+    // return all the information. Else: Access Denied
+    if (idExists._id == reqUserId) return res.status(200).send(idExists)
+    else return res.status(401).send("Access Denied")
+  } catch (err) {
+    res.status(404).send(`User does not exist`)
+  }
+
+  // if (!idExists) return res.status(404).send("UserId not found")
 })
