5f2f0a2fd2a3a13293a0817f169babf99a657904
Chris Steffes <c.steffes@live.ca>
2021-07-03 12:22:05 -0500

    begin wallet validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,58 +1,72 @@
-const router = require("express").Router()
-const User = require("../model/User")
-const { registerValidation, loginValidation } = require("../utils/validation")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+const router = require("express").Router();
+const User = require("../model/User");
+const { registerValidation, loginValidation } = require("../utils/validation");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Registration
 router.post("/register", async (req, res) => {
   // Validation
-  const { error } = registerValidation(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   // Check if user alerady exists
   const emailExists = await User.findOne({
-    email: req.body.email
-  })
-  if (emailExists) return res.status(400).send("Email already exists!")
+    email: req.body.email,
+  });
+  if (emailExists) return res.status(400).send("Email already exists!");
 
   // Hash the password
-  const salt = await bcrypt.genSalt(10)
-  const hashPassword = await bcrypt.hash(req.body.password, salt)
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
   // Create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: hashPassword,
-    walletAddress: req.body.walletAddress
-  })
+    walletAddress: req.body.walletAddress,
+  });
   try {
-    const savedUser = await user.save()
-    res.send({ user: user._id })
+    const savedUser = await user.save();
+    res.send({ user: user._id });
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send(err);
   }
-})
+});
 
 // Login
 router.post("/login", async (req, res) => {
   // Validation
-  const { error } = loginValidation(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   // Check if user alerady exists
-  const user = await User.findOne({ $or: [{ email: req.body.email }, { walletAddress: req.body.walletAddress }] }).exec(async function (err, user) {
+  const user = await User.findOne({
+    $or: [{ email: req.body.email }, { walletAddress: req.body.walletAddress }],
+  }).exec(async function (err, user) {
     //no users with that email NOR walletAddress exist.
-    if (!user) return res.status(400).send("Login incorrect.")
+    if (!user) return res.status(400).send("Login incorrect.");
     //user already exists with email AND/OR walletAddress.
-    const validPass = await bcrypt.compare(req.body.password, user.password)
-    if (!validPass) return res.status(400).send("Password incorrect")
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send("Password incorrect");
     // Upon Successful Login
     // Create and assign a token
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
-    res.header("auth-token", token).send(token)
-  })
-})
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).send(token);
+  });
+});
 
-module.exports = router
+router.post("/verifyWallet", async (req, res) => {
+  // Validation
+  const { error } = walletValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  // Check if walletAddress Exists
+  const userByWallet = await User.findOne({
+    walletAddress: req.body.walletAddress,
+  });
+  // No users with that walletAddress exist
+  if (!User) return res.status(400).send("Wallet Address Invalid");
+});
+
+module.exports = router;
--- utils/validation.js
+++ utils/validation.js
@@ -1,5 +1,5 @@
 // Validation
-const Joi = require("@hapi/joi")
+const Joi = require("@hapi/joi");
 
 /* -> TODO <-
   [] - regis+login Change to validate wallet address
@@ -13,21 +13,31 @@ const registerValidation = data => {
     name: Joi.string().min(5).required(),
     email: Joi.string().min(6).required().email(),
     walletAddress: Joi.string().min(42).required(),
-    password: Joi.string().min(20).required()
-  })
+    password: Joi.string().min(20).required(),
+  });
   // Validate before making a user
-  return joiSchema.validate(data)
-}
+  return joiSchema.validate(data);
+};
 
+// Login Validation
 const loginValidation = data => {
   const joiSchema = Joi.object({
     email: Joi.string().min(6).email(),
     walletAddress: Joi.string().min(42),
-    password: Joi.string().min(20).required()
-  }).xor("email", "walletAddress")
+    password: Joi.string().min(20).required(),
+  }).xor("email", "walletAddress");
   // Validate before logging in user
-  return joiSchema.validate(data)
-}
+  return joiSchema.validate(data);
+};
 
-module.exports.registerValidation = registerValidation
-module.exports.loginValidation = loginValidation
+// Login Validation
+const walletValidation = data => {
+  const joiSchema = Joi.object({
+    walletAddress: Joi.string().min(42),
+  });
+  // Validate before logging in user
+  return joiSchema.validate(data);
+};
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
+module.exports.walletValidation = walletValidation;
