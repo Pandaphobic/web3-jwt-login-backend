386738309b7a45efcda424330258c23db6799ca5
Chris Steffes <c.steffes@live.ca>
2021-07-03 16:42:16 -0500

    test commit

--- routes/walletLogin.js
+++ routes/walletLogin.js
@@ -10,6 +10,6 @@ module.exports = function auth(req, res, next) {
     // req.user = verified;
     next();
   } catch (err) {
-    // res.status(400).send("Invalid Token");
+     res.status(400).send("Invalid Token");
   }
 };
